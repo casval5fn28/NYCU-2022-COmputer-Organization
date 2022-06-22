22bd24a5e08cf6e8bac53a749eb3b532fc770bee
Casval_dreamwalker <71252273+casval5fn28@users.noreply.github.com>
2022-06-23 02:08:28 +0800

    Add files via upload

---
+++ Lab6_release/direct_mapped_cache.cpp
@@ -0,0 +1,68 @@
+#include "direct_mapped_cache.h"
+#include "string"
+#include <fstream>
+#include <bitset>
+#include <unordered_map>
+
+using namespace std;
+
+string ToBinary(string& str) {
+    string binary;
+    int len = str.size();
+    for (int i = 0; i < len; ++i) {
+        if (str[i] >= 'a' && str[i] <= 'f') {
+            binary += bitset<4>((str[i] - 'a' + 10)).to_string();
+        } else {
+            binary += bitset<4>((str[i] - '0')).to_string();
+        }
+    }
+    if (binary.size() == 28) {
+        binary = "0000" + binary;
+    }
+    return binary;
+}
+
+int BitNum(int num) {
+    int n = 0;
+    while (num >= 2) {
+        num /= 2;
+        n++;
+    }
+    return n;
+}
+
+float direct_mapped(string filename, int block_size, int cache_size) {
+    int total_num = 0;
+    int hit_num = 0;
+
+    /*write your code HERE*/
+    ifstream file;
+    string str;
+    int byte_offset = BitNum(block_size);
+    int index = BitNum((cache_size / block_size));
+    int tag = 32 - byte_offset - index;
+    unordered_map<string, string> cache;
+    file.open(filename);
+    while (file >> str) {
+        if (file.eof()) {
+            break;
+        }
+        string bit_str = ToBinary(str);
+        string tag_str(bit_str, 0, tag);
+        string index_str(bit_str, tag, index);
+        if (cache.count(index_str)){
+            if (cache[index_str] == tag_str){
+                hit_num++;
+            }
+            else{
+                cache[index_str] = tag_str;
+            }
+        }
+        else{
+            cache[index_str] = tag_str;
+        }
+        total_num++;
+    }
+    file.close();
+    return (float)hit_num / total_num;
+}
---
+++ Lab6_release/set_associative_cache.cpp
@@ -0,0 +1,68 @@
+#include "set_associative_cache.h"
+#include "string"
+#include <fstream>
+
+using namespace std;
+
+int BitNum_sec(int num) {
+    int n = 0;
+    while (num >= 2) {
+        num /= 2;
+        n++;
+    }
+    return n;
+}
+
+float set_associative(string filename, int way, int block_size, int cache_size){
+    int total_num = 0;
+    int hit_num = 0;
+
+    /*write your code HERE*/
+    ifstream file;
+    string str;
+    int byte_offset = BitNum_sec(block_size);
+    int idx_range = cache_size / (block_size*way);
+    int index = BitNum_sec(idx_range);
+    int cache[idx_range][way];
+    int used_block[idx_range]={};
+
+    file.open(filename);
+    while (file >> str) {
+        if (file.eof()) {
+            break;
+        }
+        bool hit = false;
+        unsigned int bit_str = strtoul(str.c_str(), NULL, 16);
+        unsigned int idx = (bit_str>> byte_offset) % idx_range;
+        unsigned int tag = bit_str >> (byte_offset + index);
+
+        for(int i = 0; i < used_block[idx]; i++){
+            if(cache[idx][i] == tag){
+                for(int j = i; j < used_block[idx]-1; j++){
+                    cache[idx][j] = cache[idx][j+1];
+                }
+                cache[idx][used_block[idx]-1] = tag;
+                hit_num++;
+                hit = true;
+                break;
+            }
+        }
+
+        if(hit == false){
+            if(used_block[idx] == way){
+                for(int j = 0; j < way-1; j++){
+                    cache[idx][j] = cache[idx][j+1];
+                }
+                cache[idx][way-1] = tag;
+            }
+            else{
+                cache[idx][used_block[idx]] = tag;
+                used_block[idx]++;
+            }
+        }
+        total_num++;
+    }
+
+    file.close();
+    return (float)hit_num/total_num;
+}
